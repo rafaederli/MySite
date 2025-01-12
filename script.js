201ba03498c972760dc4b75f6366f479cab82111
Rafael Ederli <rafaederli@gmail.com>
2025-01-12 20:12:44 -0300

    main content development

--- script.js
+++ script.js
@@ -1,55 +1,9 @@
 const header = document.querySelector("header")
 const divider = document.getElementById("divider")
+const main = document.querySelector("main")
+
 header.addEventListener("click", () => {
     header.classList.add("clicked")
     divider.classList.add("divider")
+    main.classList.add("visible")
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-const infos = document.getElementById("infos")
-
-infos.addEventListener("click", () => {
-    infos.style.width = "25%"
-    infos.style.paddingLeft = "2.5rem"
-    infos.style.paddingTop = "5.5rem"
-})
-*/
-/*.infos {
-    position: fixed;
-    width: 25%;
-    padding-left: 2.5rem;
-    padding-top: 5.5rem;
-}*/
-
-/*
-.infos h1 {
-    font-family: "Poppins", serif;
-    font-weight: 500;
-    font-size: 1.5rem;
-}
-
-.infos h2 {
-    font-family: "Poppins", serif;
-    font-weight: 300;
-    font-size: 0.8rem;
-}*/
